7ec72505d87314507a083910b31d29471a815a5f
Mark Asfour <mj4sparrow@gmail.com>
2015-11-22 13:12:05 -0800

    added filestream

--- main.cpp
+++ main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-	cout << argc << endl;
-	for (int i = 0; i < argc; i++)
+	cout << "Welcome to Mark Asfour's Feature Selection Algorithm." << endl;
+	cout << "Type in the name of the file to test: ";
+	string filename;
+	cin >> filename;
+	
+	string line;
+	ifstream file(filename.c_str());
+	if (file.is_open())
 	{
-		cout << argv[i] << endl;
+		while (getline(file, line))
+		{
+			cout << line << endl;
+		}
+		file.close();
 	}
+	else
+	{
+		cout << "Unable to open file" << endl;
+		return 0;
+	}
+
 	return 0;
 }
